d71b44ae54b5edd59d238196df14595d12bffa7e
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-09-14 12:06:18 -0300

    Configura cabeçalho da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Separator } from "@radix-ui/react-separator"
 import { Button } from "./components/ui/button"
+import { Github } from 'lucide-react'
 
 function App() {
   return (
     <>
-      <h1>Hello, World! PHCA is here!!!</h1>
-      <Button>Teste</Button>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">Upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido 💛 no NLW/Rockeseat por PHCA-dev 
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="secondary">
+            <Github className="w-4 h-4 mr-2" />
+            GitHub
+          </Button>
+        </div>
+      </div>
+      
     </>
   )
 }
